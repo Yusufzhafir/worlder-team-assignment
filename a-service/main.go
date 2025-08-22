3b9f32e5b4927516056615fa19fa406ecc2dcd1a
yusuf shadiq <70932237+Yusufzhafir@users.noreply.github.com>
2025-08-22 16:36:47 +0700

    initial commit

---
+++ a-service/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	pb "github.com/Yusufzhafir/worlder-team-assignment/common/protobuf"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+var (
+	address = "localhost:50051"
+)
+
+func main() {
+	conn, err := grpc.NewClient(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+		return
+	}
+	defer conn.Close()
+	c := pb.NewIngestServiceClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	ack, err := c.StreamReadings(ctx, &pb.SensorReading{
+		Value:       10,
+		SensorType:  "manuel",
+		Id1:         "asldk",
+		Id2:         10,
+		TimestampMs: time.Now().Unix(),
+	})
+
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Status Result: %s", ack.GetStatus())
+}
---
+++ b-service/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	pb "github.com/Yusufzhafir/worlder-team-assignment/common/protobuf"
+	"google.golang.org/grpc"
+)
+
+var (
+	port = 50051
+)
+
+type ServerGRPC struct {
+	pb.UnimplementedIngestServiceServer
+}
+
+// SayHello implements helloworld.GreeterServer
+func (s *ServerGRPC) StreamReadings(_ context.Context, in *pb.SensorReading) (*pb.StreamAck, error) {
+	log.Printf("Received: %v", in.GetId1())
+	return &pb.StreamAck{
+		Status: fmt.Sprintf("successfully received id %v", in),
+	}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterIngestServiceServer(s, &ServerGRPC{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
